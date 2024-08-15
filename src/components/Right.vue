4f2a60f2ec96a7abbca8d10abc14556abdc6279f
AshishGowtham <122021623+AshishGowtham@users.noreply.github.com>
2024-08-15 19:04:28 +0530

    added scenario UI

--- src/components/Right.vue
+++ src/components/Right.vue
@@ -4,17 +4,67 @@
     <img :src="steering_wheel" />
   </div>
 
-  <div v-if="rightState === 'diff_sce'" class="w-full h-screen">
-    <div class="flex flex-col items-center h-screen justify-center gap-y-4">
+  <div v-if="rightState === 'diff_sce'" class="w-full h-[5000px]">
+
+    <div class="flex flex-col items-center gap-y-4 py-12">
       <div v-if="selectedDifficulty" class="flex gap-x-2">
         <h1 class="text-4xl text-white text-center">You are choosing the difficulty: </h1>
         <h1 class="text-4xl text-white text-center capitalize font-semibold">{{ selectedDifficulty }}</h1>
       </div>
       <div v-if="selectedScenario" class="flex gap-x-2">
         <h1 class="text-4xl text-white text-center">You are choosing the scenario: </h1>
-        <h1 class="text-4xl text-white text-center capitalize font-semibold">{{ selectedScenario }}</h1>
+        <h1 class="text-4xl text-white text-center capitalize font-semibold">{{ selectedScenario }}</h1> 
       </div>
     </div>
+
+    <!-- scene 1 and scene 3 -->
+    <!-- FLV and DOC -->
+    <div v-if="selectedScenario === 'scene1' || selectedScenario === 'scene3'" class="flex justify-evenly">
+
+      <div>
+        <img :src="FLV" class="w-[40rem]" />
+        <h1 class="text-4xl text-yellow text-center py-4">Follow Leading Vehicle</h1>
+      </div>
+
+      <div>
+        <img :src="DOC" class="w-[40rem]" />
+        <h1 class="text-4xl text-yellow text-center py-4">Dynamic Object Crossing</h1>
+      </div>
+      
+    </div>
+
+    <!-- scene 2 -->
+     <!-- CL, NSLT, UT, OVRRL, CSC -->
+
+    <div v-if="selectedScenario === 'scene2'" class="grid grid-cols-3 gap-4">
+
+      <div>
+        <img :src="CSC" class="w-[40rem]" />
+        <h1 class="text-4xl text-yellow text-center py-4">Construction Setup Crossing</h1>
+      </div>
+  
+      <div>
+        <img :src="CL" class="w-[40rem]" />
+        <h1 class="text-4xl text-yellow text-center py-4">Control Loss</h1>
+      </div>
+  
+      <div>
+        <img :src="NSLT" class="w-[40rem]" />
+        <h1 class="text-4xl text-yellow text-center py-4">Non Signalized Left Turn</h1>
+      </div>
+      
+      <div>
+        <img :src="OVRRL" class="w-[40rem]" />
+        <h1 class="text-4xl text-yellow text-center py-4">Opposite Vehicle Running Red Light</h1>
+      </div>
+      
+      <div>
+        <img :src="UT" class="w-[40rem]" />
+        <h1 class="text-4xl text-yellow text-center py-4">Uphill Traffic</h1>
+      </div>
+
+    </div>
+
   </div>
 
   <div class="container mx-auto" v-if="rightState === 'towns'">
@@ -105,6 +155,14 @@
 <script setup>
   import { ref, onMounted } from 'vue';
   import steering_wheel from '../assets/steering_wheel.png';
+
+  import CSC from "../assets/scenes/Construction_Setup_Crossing.gif";
+  import CL from "../assets/scenes/Control_Loss.gif";
+  import DOC from "../assets/scenes/Dynamic_Object_Crossing.gif";
+  import FLV from "../assets/scenes/Follow_Leading_Vehicle.gif";
+  import NSLT from "../assets/scenes/Non_Signalized_Left_Turn.gif";
+  import OVRRL from "../assets/scenes/Opposite_Vehicle_Running_Red_Light.gif";
+  import UT from "../assets/scenes/Uphill_Traffic.gif";
 
   const rightState = ref('landing');
   const selectedDifficulty = ref('');
